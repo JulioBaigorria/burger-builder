0ebede828e9007beddaedf73178b461dc5078e0a
unknown <julio.baigorria1@gmail.com>
2020-01-02 22:15:17 -0300

    haciendo builder dinamico

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,8 +3,20 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient'
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        console.log(igKey)
+        console.log()
+        return [...Array(props.ingredients[igKey])].map((_, i)=>
+    {   
+        
+        console.log(i)
+        return <BurgerIngredient key={igKey + i} type={igKey}/>
+        
+        })
+    })
+    
     return (
-        <div className={classes.Burger}>
+        <div className={classes.Burger} >
             <BurgerIngredient type='bread-top' />
             <BurgerIngredient type='cheese' />
             <BurgerIngredient type='meat' />
--- src/components/Burger/BurgerIngredients/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredient.js
@@ -1,43 +1,42 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import ingredients from './BurgerIngredient.css'
 
 
-const burgerIngredient = (props) => {
-    let ingredient = null
+class BurgerIngredient extends Component {
+    render(){
+        let ingredient = null
 
-    switch(props.type){
-        case ('bread-bottom'):
-            ingredient = <div ClassName={ingredients.BreadBottom}></div>
-            break
-        case ('bread-top'):
-            ingredient = (
-                <div className={ingredients.BreadTop}>
-                   <div className={ingredients.Seeds1}></div>
-                   <div className={ingredients.Seeds2}></div>
-                </div>
-           ) 
-           break
-        case ('meat'):
-            ingredient = <div className={ingredients.Meat}></div>
-            break
-        case ('cheese'):
-            ingredient = <div className={ingredients.Cheese}></div>
-            break
-        case ('salad'):
-            ingredient = <div className={ingredients.Salad}></div>
-            break
-        case ('bacon'):
-            ingredient = <div className={ingredients.Bacon}></div>
-            break
-        default: ingredient = null
-    }
+        switch (this.props.type) {
+            case ('bread-bottom'):
+                ingredient = <div className={ingredients.BreadBottom}></div>
+                break
+            case ('bread-top'):
+                ingredient = (
+                    <div className={ingredients.BreadTop}>
+                        <div className={ingredients.Seeds1}></div>
+                        <div className={ingredients.Seeds2}></div>
+                    </div>
+                )
+                break
+            case ('meat'):
+                ingredient = <div className={ingredients.Meat}></div>
+                break
+            case ('cheese'):
+                ingredient = <div className={ingredients.Cheese}></div>
+                break
+            case ('salad'):
+                ingredient = <div className={ingredients.Salad}></div>
+                break
+            case ('bacon'):
+                ingredient = <div className={ingredients.Bacon}></div>
+                break
+            default: ingredient = null
+    }   
     return ingredient
-} 
-
-/*burgerIngredient.propTypes = {
-    type: propTypes.string.isRequired
-}*/
-
-
-export default burgerIngredient
+    }      
+}   
+    BurgerIngredient.propTypes = {
+        type: PropTypes.string.isRequired
+    } 
+export default BurgerIngredient
