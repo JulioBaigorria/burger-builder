0ac13978d88f036c7fb610c3855a9f942143eab2
unknown <julio.baigorria1@gmail.com>
2020-01-03 02:18:38 -0300

    Agregado BuildControls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,23 +3,25 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient'
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-        console.log(igKey)
-        console.log()
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i)=>
-    {   
-        
-        console.log(i)
-        return <BurgerIngredient key={igKey + i} type={igKey}/>
-        
-        })
+            {   
+                return <BurgerIngredient key={igKey + i} type={igKey}/>
+            }
+        )
     })
-    
+    .reduce((arr, el) =>{
+        return arr.concat(el)
+    }, [])
+
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Pongale algo</p>
+    }
+    console.log(transformedIngredients)
     return (
         <div className={classes.Burger} >
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {transformedIngredients}  
             <BurgerIngredient type='bread-bottom'/>
         </div>
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,21 +1,32 @@
 import React,{ Component } from 'react'
 import Auxiliary from '../../hoc/Auxiliary'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 class BurgerBuilder extends Component{
     state ={
         ingredients: {
             salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
+            bacon: 2,
+            cheese: 3,
+            meat: 3
         }
     }
+
+    addIngredientHandler = (type) => {
+
+    }
+
+    removeIngredientHandler = (type) => {
+
+    }
+
+    
     render(){
         return(
             <Auxiliary>
                 <Burger  ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls />
             </Auxiliary>
         )
     }
